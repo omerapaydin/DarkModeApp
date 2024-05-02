85d4e634021b43924be3953ca5e12560359bbe9f
Ömer Apaydın <omerapaydin90@gmail.com>
2024-05-02 17:21:24 +0300

    First

--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -1,18 +1,39 @@
-//
-//  ViewController.swift
-//  DarkMode
-//
-//  Created by Ömer on 2.05.2024.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+      //kodun bulunuğu ekranı ligth/dark yapar
+      //  overrideUserInterfaceStyle = .light
+      
+        
+        
     }
+    
+
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        }else{
+            changeButton.tintColor = UIColor.blue
+
+        }
+        
+        
+        
+    }
+    
+    
+    
 
 
 }
